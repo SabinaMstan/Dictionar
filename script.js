2c3595283b417856f79f9fa851d0d4c9c508f309
SabinaMstan <93779021+SabinaMstan@users.noreply.github.com>
2022-01-09 18:17:56 +0200

    Fara alert();
    Varianta de cod fara alert pentru a afisa mesajul dupa cautarea cuvantului. Afisarea este facuta din HTML.
    
    + cand se inregistreaza cuvintele in dictionar se poate apasa Enter in loc de click pe buton.

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 let dictionary = [];
 
+document.getElementById('userInput').addEventListener('keyup', function(event) {
+  let buttonId;
+  if (event.keyCode === 13 && document.getElementById('submitWord').input.id === 'addWord') {
+    buttonId = 'submitWord';
+  } else if (event.keyCode === 13  && document.getElementById('search').input.id === 'searchWord') {
+    buttonId = 'search';
+  }
+    event.preventDefault();
+    document.getElementById(buttonId).click();
+});
+
 function getInputFromTextBox() {
-  let input = document.getElementById('userInput').value;
+  let input = document.getElementById('addWord').value;
+  document.getElementById('addWord').value = '';
   addWord(input);
-  document.getElementById('userInput').value = '';
 }
 
 function addWord(input) {
   dictionary.push(input);
-//alert(dictionary);
+  //alert(dictionary);
 }
 
 function searchWordInDictionary() {
   let word = document.getElementById('searchWord').value;
-  if (dictionary.includes(word)) {
-    alert('found');
-  } else {
-    alert ('not found');
-  }
+  let found = document.getElementById('foundStatus');
+  let foundStatus = document.createElement('p');
   document.getElementById('searchWord').value = '';
+  if (dictionary.includes(word)) {
+    foundStatus.innerHTML = `${word} - found`;
+  } else {
+    foundStatus.innerHTML = `${word} - not found`;
+  }
+  found.appendChild(foundStatus);
 }
